a1f3483807999441bbf9811f518a8f883d1e7290
Administrator <Administrator@3A39KAV894G80FJ>
2018-04-22 22:07:48 +0800

    增加注解的注释

--- eureka-server-register-center/src/main/java/com/xyl/Application.java
+++ eureka-server-register-center/src/main/java/com/xyl/Application.java
@@ -6,8 +6,12 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
  * 服务注册中心
+ * Eureka通过心跳检查、客户端缓存等机制，确保了系统的高可用性、灵活性和可伸缩性。
+ * Eureka Server提供服务注册服务，各个节点启动后，会在Eureka Server中进行注册
  * @author  xyl
- *
+ *1,@Configuration的注解类标识这个类可以使用Spring IoC容器作为bean定义的来源。@Bean注解告诉Spring，一个带有@Bean的注解方法将返回一个对象，该对象应该被注册为在Spring应用程序上下文中的bean。
+ *2、@EnableAutoConfiguration：能够自动配置spring的上下文，试图猜测和配置你想要的bean类，通常会自动根据你的类路径和你的bean定义自动配置。
+ *3、@ComponentScan：会自动扫描指定包下的全部标有@Component的类，并注册成bean，当然包括@Component下的子注解@Service,@Repository,@Controller。
  */
 @SpringBootApplication
 @EnableEurekaServer
