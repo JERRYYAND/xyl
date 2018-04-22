0a5f546dc5a03b80cdadd008a5fc44303d39aaa4
Administrator <Administrator@3A39KAV894G80FJ>
2018-04-22 18:07:53 +0800

    用于存放请求的接口

---
+++ spring-cloud-rest-center/src/main/java/com/xyl/RequestURLConstant.java
@@ -0,0 +1,10 @@
+package com.xyl;
+/**
+ * 用于存放请求的接口
+ */
+public class RequestURLConstant {
+	/**测试接口*/
+	public static final String hello_word_service= "hello-word-service";
+	public static final String hello_word_service_R = "/hello-word-service";
+
+}
