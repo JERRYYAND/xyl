c52aa6bc59832a8cf1b96fd0005d25ef2a6a21f5
Administrator <Administrator@3A39KAV894G80FJ>
2018-04-22 18:01:57 +0800

    提供者:provider>>>>>启动类

---
+++ spring-cloud-provider-center/src/main/java/com/xyl/StartProviderCenter.java
@@ -0,0 +1,23 @@
+package com.xyl;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * 提供者:provider
+ */
+@EnableEurekaClient
+@SpringBootApplication
+/**dao扫描*/
+@MapperScan("com.xyl.dao")
+public class StartProviderCenter {
+
+	public static void main(String[] args) {
+		
+		new SpringApplicationBuilder(StartProviderCenter.class).web(true).run(args);
+
+	}
+
+}
