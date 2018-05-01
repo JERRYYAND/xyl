0ac4a60a0b70e13a5d449fba0f3800f361362fd1
Administrator <Administrator@3A39KAV894G80FJ>
2018-05-01 21:33:07 +0800

    模拟远程配置中心

---
+++ spring-config-client/src/main/java/com/xyl/SpringClientApp.java
@@ -0,0 +1,14 @@
+package com.xyl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringClientApp {
+	public static void main(String[] args) {
+		
+		SpringApplication.run(SpringClientApp.class, args);
+		
+	}
+
+}
