0738fbb1cbe260605f156eb88133fabd529ed279
Administrator <Administrator@3A39KAV894G80FJ>
2018-05-07 22:32:33 +0800

    改造

---
+++ spring-cloud-service-api/src/main/java/com/xyl/dao/ProductCategoryMapper.java
@@ -0,0 +1,17 @@
+package com.xyl.dao;
+
+import com.xyl.model.ProductCategory;
+
+public interface ProductCategoryMapper {
+    int deleteByPrimaryKey(Integer categoryId);
+
+    int insert(ProductCategory record);
+
+    int insertSelective(ProductCategory record);
+
+    ProductCategory selectByPrimaryKey(Integer categoryId);
+
+    int updateByPrimaryKeySelective(ProductCategory record);
+
+    int updateByPrimaryKey(ProductCategory record);
+}
