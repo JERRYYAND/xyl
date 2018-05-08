e822babf7b1dc2e6badddf4e2a4e0aaf0f37962c
Administrator <Administrator@3A39KAV894G80FJ>
2018-05-08 22:21:38 +0800

    服务之间的调用=====feign实现

--- order/src/main/java/com/xyl/OrderApplicaton.java
+++ order/src/main/java/com/xyl/OrderApplicaton.java
@@ -4,9 +4,11 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableFeignClients
 @MapperScan("com.xyl.dao")
 public class OrderApplicaton {
 
