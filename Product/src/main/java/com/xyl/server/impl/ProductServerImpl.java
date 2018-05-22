0f36644314c8ec59f706a458e14c3a96d6ef0ed5
Administrator <Administrator@3A39KAV894G80FJ>
2018-05-22 22:02:22 +0800

    测试feign +mybatis

--- Product/src/main/java/com/xyl/server/impl/ProductServerImpl.java
+++ Product/src/main/java/com/xyl/server/impl/ProductServerImpl.java
@@ -2,23 +2,20 @@ package com.xyl.server.impl;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.xyl.dao.ProductInfoMapper;
-import com.xyl.model.ProductInfo;
 import com.xyl.server.ProductServer;
 
 
-@Service
+@Service(value="productServer")
 public class ProductServerImpl implements ProductServer{
 	private final Logger log = LoggerFactory.getLogger(this.getClass());
-	@Autowired
-	private  ProductInfoMapper productInfoMapper;
+//	@Autowired
+//	private  ProductInfoMapper productInfoMapper;
 	@Override
 	public String  selectProductInfo() {
-		// ProductInfo pc = productInfoMapper.selectByPrimaryKey("157875196366160022");
-		 //log.info("pc>>>>>>>>>>>>>>>>>>>>"+pc);
+//		 ProductInfo pc = productInfoMapper.selectByPrimaryKey("157875227953464068");
+//		 log.info("pc>>>>>>>>>>>>>>>>>>>>"+pc);
 		return "product-hello-word";
 	}
 
